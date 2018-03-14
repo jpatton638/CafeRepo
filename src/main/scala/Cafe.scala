1ea952f0269ab57ddee69e6f51db9e0b6b20114a
jpatton638 <jpatton638@gmail.com>
2018-03-14 14:26:37 +0000

    completed grind and frothMilk

--- src/main/scala/Cafe.scala
+++ src/main/scala/Cafe.scala
@@ -1,24 +1,35 @@
 object Cafe {
+
   case class Water(temperature: Double = 20.0)
   case class GroundCoffee(name: String)
-  case class Milk(name: String)
   case class Coffee(name: String, milky: Option[Milk])
+  case class FrothedMilk(milk: Milk)
+
+  trait Milk
+
+  case class WholeMilk() extends Milk
+  case class SemiSkimmedMilk() extends Milk
 
   type CoffeeBeans = String
-  type FrothedMilk = String
-
-
 
   def heat(water: Water, temperature: Double = 40.0): Water = {
     Water(temperature)
   }
 
   def grind(beans: CoffeeBeans): GroundCoffee = {
-    GroundCoffee(beans)
+    if (beans.toLowerCase == "arabica beans") {
+      GroundCoffee(beans)
+    } else {
+      throw new IllegalArgumentException
+    }
   }
 
   def frothMilk(milk: Milk): FrothedMilk = {
-    ???
+    if (milk.isInstanceOf[WholeMilk]) {
+      FrothedMilk(milk)
+    } else {
+      throw new IllegalArgumentException
+    }
   }
 
   def brew(water: Water, coffee: GroundCoffee): Coffee = {
--- src/test/scala/CafeSpec.scala
+++ src/test/scala/CafeSpec.scala
@@ -1,3 +1,4 @@
+import Cafe.{SemiSkimmedMilk, WholeMilk}
 import org.scalatest.{MustMatchers, WordSpec}
 
 class CafeSpec extends WordSpec with MustMatchers {
@@ -7,17 +8,14 @@ class CafeSpec extends WordSpec with MustMatchers {
     "heat is called" must {
 
       "return Water when called" in {
-
         Cafe.heat(Cafe.Water()) mustEqual Cafe.Water(40)
       }
 
       "return Water with default 40 if no temperature given" in {
-
         Cafe.heat(Cafe.Water()) mustEqual Cafe.Water(40)
       }
 
       "return Water with given temperature" in {
-
         Cafe.heat(Cafe.Water(), 80.0) mustEqual Cafe.Water(80)
       }
     }
@@ -25,14 +23,30 @@ class CafeSpec extends WordSpec with MustMatchers {
     "grind is called" must {
 
       "return GroundCoffee when given Arabica Beans" in {
-
         Cafe.grind("Arabica Beans") mustEqual Cafe.GroundCoffee("Arabica Beans")
       }
 
-
+      "throw a IllegalArgumentException when given anything else" in {
+        intercept[IllegalArgumentException] {
+          Cafe.grind("baked beans")
+        }
+      }
 
     }
 
+    "frothMilk is called" must {
+
+      "return frothed milk when given whole milk" in {
+        Cafe.frothMilk(new WholeMilk) mustEqual Cafe.FrothedMilk(new WholeMilk)
+      }
+
+      "throw a IllegalArgumentException when given semi skimmed milk" in {
+        intercept[IllegalArgumentException] {
+          Cafe.frothMilk(new SemiSkimmedMilk)
+        }
+      }
+
+    }
     "brew is called" must {
 
     }
